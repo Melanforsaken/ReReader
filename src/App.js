6d787a879dd86e7ef2b8b634834685973bce97c9
Melanforsaken <Mailreko02@gmail.com>
2025-09-09 13:31:46 +0200

    eindelijk de juiste paden in app.css, credits naar JJ Siewers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import DocumentList from './components/DocumentList';
+import Header from './Components/Header/Header';
+import SearchBar from './Components/SearchBar/SearchBar';
+import DocumentList from './Components/DocumentList/DocumentList';
 import './App.css';
 
 const App = () => {
