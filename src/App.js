2022cf29a220c620686fae59b1c108a1bc6f7c74
Melanforsaken <Mailreko02@gmail.com>
2025-09-09 11:09:03 +0200

    wijziging import plus export paths in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import DocumentList from './components/DocumentList';
+import Header from './components/Header/Header';
+import SearchBar from './components/SearchBar/SearchBar';
+import DocumentList from './components/DocumentList/DocumentList';
 import './App.css';
 
 const App = () => {
