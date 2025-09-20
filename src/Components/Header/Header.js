b27cdf9aa290b677cd34f25f485a51bfb6e9b68e
Melanforsaken <Mailreko02@gmail.com>
2025-09-20 19:44:24 +0200

    verwijdering toggle, minimize en close buttons in header en vergroting logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,11 +5,6 @@ const Header = () => {
     return (
         <div className="header">
             <div className="logo">ReReader</div>
-            <div className="functions">
-                <button>Toggle Off</button>
-                <button>Minimize</button>
-                <button>Close</button>
-            </div>
         </div>
     );
 };
