68c044fc49d5118bf25099946f823b1eaa69eca4
Melanforsaken <Mailreko02@gmail.com>
2025-10-06 15:50:16 +0200

    aanvulling BookItem.js code

--- src/Components/BookItem/BookItem.js
+++ src/Components/BookItem/BookItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './BookItem.css';
 
-const BookItem = ({ title, cover }) => {
+const BookItem = ({ title, cover, onSelect }) => {
   return (
-    <div className="book-item">
-      <img src={cover} alt={title} style={{ width: '100%', height: 'auto' }} />
-      <h3>{title}</h3>
+    <div className="book-item" onClick={onSelect} style={{ cursor: 'pointer' }}>
+      {cover && <img src={cover} alt={title} style={{ width: '100%', height: 'auto' }} />}
+      <h3 className="book-title">{title}</h3>
     </div>
   );
 };
