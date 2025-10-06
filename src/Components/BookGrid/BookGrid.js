ed4f6fe42fde18a7218647c57a533ea4d2c4e028
Melanforsaken <Mailreko02@gmail.com>
2025-10-06 15:53:33 +0200

    aanvulling BookGrid.js code

--- src/Components/BookGrid/BookGrid.js
+++ src/Components/BookGrid/BookGrid.js
@@ -2,11 +2,16 @@ import React from 'react';
 import BookItem from '../BookItem/BookItem';
 import './BookGrid.css';
 
-const BookGrid = ({ books }) => {
+const BookGrid = ({ books, onBookSelect }) => {
   return (
     <div className="book-grid">
       {books.map((book) => (
-        <BookItem key={book.id} title={book.title} cover={book.cover} />
+        <BookItem 
+          key={book.id} 
+          title={book.title} 
+          cover={book.cover} 
+          onSelect={() => onBookSelect(book)} // Pass the book to the onSelect handler
+        />
       ))}
     </div>
   );
